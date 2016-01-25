32145a431eaf3b218598133622c86c2518626178
民傑 <cmj_418@hotmail.com>
2016-01-25 22:56:06 +0800

    加入 ColorArrowButtonTest

--- UITest/ColorArrowButtonTest.cs
+++ UITest/ColorArrowButtonTest.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace UITest
 {
-    public partial class Form1 : Form
+    public partial class ColorArrowButtonTest : Form
     {
-        public Form1()
+        public ColorArrowButtonTest()
         {
             InitializeComponent();
         }
